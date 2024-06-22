5fd846931c42e46df483400c8b2bfccc8955664e
hamishfagg <ivdata+github@ivdata.net>
2024-06-22 17:40:22 +1200

    test pwm

--- blob.py
+++ blob.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python
 
-import alsaaudio, time, audioop
-from timeit import default_timer as timer
+# import alsaaudio, time, audioop
+from rpi_hardware_pwm import HardwarePWM
+import time
 
 # Open the device in blocking capture mode.
-inp = alsaaudio.PCM(
-  type=alsaaudio.PCM_CAPTURE,
-  rate=8000,
-  channels=1,
-  device="hw:CARD=Loopback,DEV=1",
-  periodsize=160)
-tick = 0
-while True:
-  print(f"Time: {timer() - tick}")
-  tick = timer()
-  # Read data from device
-  l,data = inp.read()
-  if l:
-    # Return the maximum of the absolute value of all samples in a fragment.
-    print(audioop.max(data, 2))
+# This setup means we get a bunch of data from alsa every 0.02s or every 50th of a second.
+# inp = alsaaudio.PCM(
+#   type=alsaaudio.PCM_CAPTURE,
+#   rate=8000,
+#   channels=1,
+#   device="hw:CARD=Loopback,DEV=1",
+#   periodsize=160)
+# tick = 0
+# while True:
+#   # Read data from device
+#   l,data = inp.read()
+#   if l:
+#     # Return the maximum of the absolute value of all samples in a fragment.
+#     print(audioop.max(data, 2))
+
+
+pwm = HardwarePWM(pwm_channel=0, hz=25_000, chip=0)
+pwm.start(50) # full duty cycle
+
+time.sleep(5)
+
+pwm.stop()
