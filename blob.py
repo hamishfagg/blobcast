692824f0636864ca0376aaadcaa68d0b4c33fd95
hamishfagg <ivdata+github@ivdata.net>
2024-06-26 11:23:27 +1200

    Add easing

--- blob.py
+++ blob.py
@@ -5,6 +5,7 @@ from rpi_hardware_pwm import HardwarePWM
 import time
 import sys
 from itertools import cycle
+from easing_functions import QuadEaseInOut
 
 # Open the device in blocking capture mode.
 # This setup means we get a bunch of data from alsa every 0.02s or every 50th of a second.
@@ -23,17 +24,24 @@ from itertools import cycle
 #     print(audioop.max(data, 2))
 
 def idle(pwm):
-    IDLE_CYCLE_TIME = 1.0  # Seconds
+    IDLE_CYCLE_TIME = 3.0  # Seconds
     IDLE_CYCLE_STEPS = 10  # Steps in each direction. So total steps is 2* this number
-    IDLE_MAX_DUTY = 60
-    IDLE_MIN_DUTY = 30
+    IDLE_MAX_DUTY = 80
+    IDLE_MIN_DUTY = 60
+
+    easing = QuadEaseInOut(start=IDLE_MIN_DUTY, end=IDLE_MAX_DUTY, duration=IDLE_CYCLE_STEPS)
 
     pwm.change_frequency(60)
     pwm.start(IDLE_MAX_DUTY)
     desc_range = range(IDLE_MAX_DUTY, IDLE_MIN_DUTY, -int((IDLE_MAX_DUTY-IDLE_MIN_DUTY)/IDLE_CYCLE_STEPS))
     asc_range = range(IDLE_MIN_DUTY, IDLE_MAX_DUTY, int((IDLE_MAX_DUTY-IDLE_MIN_DUTY)/IDLE_CYCLE_STEPS))
-    print([*desc_range, *asc_range])
-    for duty in cycle([*desc_range, *asc_range]):
+    
+    steps = [*desc_range, *asc_range]
+    print(steps)
+    eased_steps = list(map(easing, steps))
+    print(eased_steps)
+
+    for duty in cycle(eased_steps):
         time.sleep(IDLE_CYCLE_TIME/(IDLE_CYCLE_STEPS*2))
         pwm.change_duty_cycle(duty)
 
